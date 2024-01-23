80bc4632e2c5d7199b544da580ffdc16538ab88a
gwonsang <gwonsang247@gmail.com>
2024-01-24 00:57:31 +0900

    [feat]토큰관리/로그아웃

---
+++ src/main/java/com/dmarket/service/LogoutService.java
@@ -0,0 +1,29 @@
+package com.dmarket.service;
+
+import com.dmarket.jwt.JWTUtil;
+import com.dmarket.repository.user.RefreshTokenRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService implements LogoutHandler {
+
+
+    private final JWTUtil jwtUtil;
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response, org.springframework.security.core.Authentication authentication) {
+        String authorization = request.getHeader("Authorization");
+        String token = authorization.split(" ")[1];
+        String email = jwtUtil.getEmail(token);
+
+        refreshTokenRepository.deleteByUserEmail(email);
+    }
+}
+
