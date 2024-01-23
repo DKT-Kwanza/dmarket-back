38d26f42126b9464401816320fc207737ea9eb16
gustjdw <johs710@gachon.ac.kr>
2024-01-23 17:41:49 +0900

    [chore] 반품 상태 수정

--- src/main/java/com/dmarket/constant/ReturnState.java
+++ src/main/java/com/dmarket/constant/ReturnState.java
@@ -4,5 +4,4 @@ public enum ReturnState {
     RETURN_REQUEST,         // 반품 요청     OrderDetailState의 RETURN_REQUEST와 다르니 주의바랍니다
     COLLECT_ING,            // 수거중
     COLLECT_COMPLETE,       // 수거 완료
-    RETURN_COMPLETE         // 반품 완료     OrderDetailState의 RETURN_REQUEST와 다르니 주의바랍니다
 }
