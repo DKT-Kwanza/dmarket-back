65d5ac57f10e6680919dbdce4eed2e67155e4821
HYG <yungi981026@gmail.com>
2024-02-04 09:34:33 +0900

    [feat] #168 버그 픽스 이슈

--- src/main/java/com/dmarket/dto/request/ProductReqDto.java
+++ src/main/java/com/dmarket/dto/request/ProductReqDto.java
@@ -8,11 +8,11 @@ import org.hibernate.validator.constraints.URL;
 import java.util.List;
 
 @Data
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProductReqDto {
 
-    @NotNull
     private Long productId;
 
     @NotEmpty
