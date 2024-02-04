52171407ad5b3693b6a4f007f2fe4cbb7297dae8
xxng1 <woongaa1@naver.com>
2024-02-04 22:55:37 +0900

    [mod] #121 알림 내림차순정렬

--- src/main/java/com/dmarket/notification/NotificationRepository.java
+++ src/main/java/com/dmarket/notification/NotificationRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     // 유저 별 알림 조회
-    List<Notification> findByReceiver(Long receiver);
+    List<Notification> findByReceiverOrderByNotificationCreatedDateDesc(Long receiver);
 
     Long countByReceiverAndIsRead(Long userId, boolean isRead);
 }
