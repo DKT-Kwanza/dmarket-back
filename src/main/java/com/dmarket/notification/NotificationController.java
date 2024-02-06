c77f6e00c390e4c66790f02103581cc54aeed997
yoon520 <gmlwjddbs123@naver.com>
2024-02-06 09:25:50 +0900

    [feat] 전체 알림 조회, 삭제 api

--- src/main/java/com/dmarket/notification/NotificationController.java
+++ src/main/java/com/dmarket/notification/NotificationController.java
@@ -1,6 +1,9 @@
 package com.dmarket.notification;
 
 import com.dmarket.dto.response.CMResDto;
+import com.dmarket.exception.ErrorCode;
+import com.dmarket.jwt.JWTUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,12 +14,14 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/notify")
+@RequestMapping("/api/notification")
 public class NotificationController {
+    private final JWTUtil jwtUtil;
     private final NotificationService notificationService;
 
     // sse 연결
@@ -28,7 +33,7 @@ public class NotificationController {
     }
 
     // 유저 별 알림 조회
-    @GetMapping("/{userId}/notifications")
+    @GetMapping("/{userId}")
     public ResponseEntity<List<Notification>> getUserNotifications(@PathVariable Long userId){
         List<Notification> notifications = notificationService.getUserNotifications(userId);
         return new ResponseEntity<> (notifications, HttpStatus.OK);
@@ -41,6 +46,18 @@ public class NotificationController {
         return new ResponseEntity<>(CMResDto.successNoRes(), HttpStatus.OK);
     }
 
+    // 알림 전체 읽음 처리
+    @PutMapping("/{userId}")
+    public ResponseEntity<?> readAllNotifications(HttpServletRequest request,
+                                                  @PathVariable Long userId) {
+        ResponseEntity<?> authorization = checkAuthorization(userId, request);
+        if(authorization != null){
+            return authorization;
+        }
+        notificationService.readAllNotifications(userId);
+        return new ResponseEntity<>(CMResDto.successNoRes(), HttpStatus.OK);
+    }
+
     // 안읽은 알림 개수
     @GetMapping("/{userId}/unreadCount")
     public ResponseEntity<?> getUnreadCount(@PathVariable Long userId) {
@@ -50,5 +67,30 @@ public class NotificationController {
         } catch (Exception e) {
             return ResponseEntity.badRequest().body("알림 카운트 오류");
         }
+    }
+
+    // 알림 일괄 삭제
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<?> deleteNotifications(HttpServletRequest request,
+                                                 @PathVariable Long userId){
+        ResponseEntity<?> authorization = checkAuthorization(userId, request);
+        if(authorization != null){
+            return authorization;
+        }
+        notificationService.deleteAllNotifications(userId);
+        return new ResponseEntity<>(CMResDto.successNoRes(), HttpStatus.OK);
+    }
+
+    // 인증 및 권한 검사 메서드
+    private ResponseEntity<?> checkAuthorization(Long userId, HttpServletRequest request) {
+        String authorization = request.getHeader("Authorization");
+        System.out.println("userId = " + userId);
+        String token = authorization.split(" ")[1];
+        Long tokenUserId = jwtUtil.getUserId(token);
+        System.out.println("tokenUserId = " + tokenUserId);
+        if (!Objects.equals(tokenUserId, userId)) {
+            return new ResponseEntity<>(CMResDto.errorRes(ErrorCode.FORBIDDEN), HttpStatus.FORBIDDEN);
+        }
+        return null; // 인증 및 권한 검사가 성공한 경우
     }
 }
--- src/main/java/com/dmarket/notification/NotificationRepository.java
+++ src/main/java/com/dmarket/notification/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.dmarket.notification;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,5 +12,14 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     // 유저 별 알림 조회
     List<Notification> findByReceiverOrderByNotificationCreatedDateDesc(Long receiver);
 
+    // 특정 유저 알림 전체 읽음 처리
+    @Modifying
+    @Query("update Notification n set n.isRead = true where n.receiver = :userId")
+    void readAllNotification(Long userId);
+
     Long countByReceiverAndIsRead(Long userId, boolean isRead);
+
+    // 특정 유저 알림 전체 삭제
+    @Modifying
+    void deleteAllByReceiver(Long userId);
 }
